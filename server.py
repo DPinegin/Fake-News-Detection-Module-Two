5730fe015d75fbf30fda6cc954be7d9b4dca4ae9
DPinegin <113193168+DPinegin@users.noreply.github.com>
2023-06-13 21:56:12 -0500

    Add files via upload

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import *  
+from flask import *
 import numpy as np
 import pandas as pd
 import os
@@ -7,10 +7,13 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
         print(os.path.join(dirname, filename))
 import numpy as np
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import nltk
 from sklearn.preprocessing import LabelBinarizer
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
+from wordcloud import WordCloud,STOPWORDS
 from nltk.stem import WordNetLemmatizer
 from nltk.tokenize import word_tokenize,sent_tokenize
 from bs4 import BeautifulSoup
@@ -26,13 +29,13 @@ from keras.models import Sequential
 from keras.layers import Dense,Embedding,LSTM,Dropout
 from keras.callbacks import ReduceLROnPlateau
 import tensorflow as tf
-from keras.models import load_model
 nltk.download('stopwords')
+import json
 
 
 app = Flask(__name__) 
 
-model = tf.keras.models.load_model(r"C:\Users\Drdr\Desktop\Fake News Detection\extension\server\saved_model.pb")
+model = tf.keras.models.load_model(r"C:\Users\Drdr\Desktop\Fake News Detection\extension\server\erast.h5")
 
 #route to the popup.html
 @app.route('/')  
@@ -48,34 +51,33 @@ def prediction():
       #swaps the key and value pairs in the recieved dictionary
       text_swap = {v: k for k, v in recieved.items()}
 
-      text = text_swap['']
-      print(text)
+      capture = text_swap['']
+
+      #df1 = pd.DataFrame({"text": [capture]})
+      
+      x = 1
 
       return ('', 204)
-
-#route to the python script. This is where the data is sent to
-#@app.route('/predict', methods=['POST', 'GET'])
-#def create_file():
- #  if request.method == 'POST':
-  #    recieved = request.form.to_dict()
-
-      #swaps the key and value pairs in the recieved dictionary
-   #   text_swap = {v: k for k, v in recieved.items()}
-
-    #  text = text_swap['']
+      if x == 1:
+         return render_template('true.html')
 
 
-     # print(text)
-      #return ('', 204)
-
-      
 
 @app.route('/success', methods = ['POST', 'GET'])  
 def print_data():  
    if request.method == 'POST':  
       result = request.form
       #return render_template("true.html",result = result)
-      return render_template("false.html",result = result)
+      #return render_template("false.html", result = result)
+      return redirect(url_for('cool_form'))
+
+@app.route('/true', methods = ['POST', 'GET'])  
+def cool_form():  
+   #if request.method == 'POST':  
+      #result = request.form
+      #return render_template("true.html",result = result)
+   return render_template('false.html')
+   return ('', 204)      
    
    
 if __name__ == '__main__':  
